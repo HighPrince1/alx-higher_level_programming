8b92fe6eba3391beb255d36b32046e9787f5f78a
HighPrince1 <odomprincewill@gmaill.com>
2023-01-11 13:28:11 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """This module defines a text file-reading function"""
 
 
- def read_file(filename=""):
-     """Prints the contents of a UTF8 text file"""
-     with open(filename, encoding="utf-8") as f:
-         print(f.read(), end="")
+def read_file(filename=""):
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
