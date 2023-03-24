571c1f0cc1ba952a2a3e692d3a5908d755deaadb
HighPrince1 <odomprincewill@gmaill.com>
2023-03-24 13:54:29 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
 """
-This script lists all states with
-a `name` starting with the letter `N`
-from the database `hbtn_0e_0_usa`.
+This is a script that  lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 """
 
 import MySQLdb as db
 from sys import argv
 
-"""
-Access to the database and get the states
-from the database.
-"""
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+            ORDER BY states.id ASC")
+    row_selected = db_cursor.fetchall()
 
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-                ORDER BY states.id ASC")
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
+    for row in row_selected:
         print(row)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script defines a State class and
-a Base class to work with MySQLAlchemy ORM.
+This is a script that defines a State class and a Base class to work 
+with MySQLAlchemy ORM.
 """
 
 from sqlalchemy import Column, Integer, String
@@ -13,7 +13,7 @@ Base = declarative_base()
 class State(Base):
     """State class
     Attributes:
-        __tablename__ (str): The table name of the class
+        __tablename__ (str): The tablename of the class
         id (int): The State id of the class
         name (str): The State name of the class
     """
